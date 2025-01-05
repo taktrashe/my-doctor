b4a025cd7f06d0bc2b4139b5af71bf632bcbde0d
ali Asa <38753563+taktrashe@users.noreply.github.com>
2025-01-05 21:52:44 +0330

    add some 1 to 105 box

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,18 @@
 import { ReactElement } from "react";
 import styles from './page.module.css';
 
+const items = Array(105)
+.fill(null)
+.map((_,i)=>i+1);
+
 export default function page(): ReactElement {
     return (
       <div className={styles.page}>
-      <div className={styles.filters}></div>
-      <div className={styles.results}></div>
+      <div className={styles.filters}>filter</div>
+      <ul className={styles.results}>
+      {items.map((item)=>(
+        <li key={item}>{item}</li>
+      ))}
+      </ul>
       </div>
 )}  
